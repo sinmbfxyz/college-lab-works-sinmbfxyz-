9aa778e6ed8b7ad636e2001e5bf8392ae18243a1
student <lostsinmbf@gmail.com>
2024-08-29 16:25:03 +0545

    Update file

--- BIM 2nd sem/Recursion/BinarySearchUsingRecusion.c
+++ BIM 2nd sem/Recursion/BinarySearchUsingRecusion.c
@@ -1,36 +1,31 @@
 #include<stdio.h>
 
-int BinarySearch(int arr[],int low, int high, int key){
-	if(low<=high){
+int BinarySearch(int arr[],int low, int high, int key) {
+	if(low<=high) {
 		int mid=low+(high-low)/2;
-		if(arr[mid]==key){
+		if(arr[mid]==key) {
 			return mid;
-		}
-		else if(key<arr[mid]){
+		} else if(key<arr[mid]) {
 			return BinarySearch(arr,low,mid-1,key);
-		}
-		else{
+		} else {
 			return BinarySearch(arr,mid+1,high,key);
 		}
-	}
-	else{
+	} else {
 		return -1;
 	}
 }
 
-void main(){
-	int arr[]={1,2,5,9,15,17,29,50,89,100};
+void main() {
+	int arr[]= {1,2,5,9,15,17,29,50,89,100};
 	int size,res,key,low=0,high;
 	size=sizeof(arr)/sizeof(arr[0]);
 	high=size-1;
 	printf("Enter key to search: ");
 	scanf("%d",&key);
 	res=BinarySearch(arr,low,high,key);
-	if(res==-1){
-		
+	if(res==-1) {
 		printf("%d not found in array",key);
-	}
-	else{
+	} else {
 		printf("%d found at index %d of array",key,res);
 	}
 }
